45a1dc18191331cb733f17576b13160ff3e467d9
koneki88 <114171959+koneki88@users.noreply.github.com>
2022-10-09 18:20:20 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,61 @@
+//1 
+ /*int hp;
+ cin >> hp;
+ bool a;
+ a = hp > 0;
+ cout << boolalpha << a;*/
+ //2 
+///int a;
+//cin >> a;
+//if (a > 20) {
+	//cout << "on";
+//}
+//else {
+	//cout << "off";  .
+
+
+// 3
+/*int tempremote,temproom,m1;
+cin >> tempremote >> temproom >> m2;
+if ((tempremote > temproom) and (m1<80)){
+ cout << "on";
+}
+else {
+ cout << "off";
+}*/
+
+
+
+
+
+//}
+
+//4
+//int main() {
+	//setlocale(LC_ALL, "Russkiy");
+	//double y, x;
+	//cin >> x;
+	//if (x > 0) {
+	//y = sqrt(x - 2);
+	//}
+	//if (x = 0) {
+	//	y = 0;
+	//}
+	//if (x < 0) {
+	//	y = abs(0);
+	//}
+	//cout << y << endl;
+
+
+//5 
+ /*int lss;
+ cin >> lss;
+ if (lss > 1 and lss < 5) {
+  switch (pal) {
+  case 1:cout << "большой" << endl; break;
+  case 2:cout << "указательный" << endl; break;
+  case 3:cout << "средний" << endl; break;
+  case 4:cout << "безымянный" << endl; break;
+  case 5:cout << "мизинец" << endl; break;
+  }
+ }
